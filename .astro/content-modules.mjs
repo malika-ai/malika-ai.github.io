4a191a04ba09eb5c7617be6758cab3024b08b91d
saddansyah <saddanakbar@gmail.com>
2025-04-10 20:13:25 +0700

    add: completed search section

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/general/malika-cekat.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgeneral%2Fmalika-cekat.mdx&astroContentModuleFlag=true")],
+["src/content/docs/tools/search.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftools%2Fsearch.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/general/malika-tools.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgeneral%2Fmalika-tools.mdx&astroContentModuleFlag=true")],
-["src/content/docs/tools/search.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftools%2Fsearch.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/general/malika-cekat.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgeneral%2Fmalika-cekat.mdx&astroContentModuleFlag=true")],
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
