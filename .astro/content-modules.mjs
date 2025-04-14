a8836a25e525c7ca2f589f2de3edfa86814f1d2d
saddansyah <saddanakbar@gmail.com>
2025-04-14 21:39:32 +0700

    fix: jubelio register

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/docs/general/malika-cekat.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgeneral%2Fmalika-cekat.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/general/malika-cekat.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgeneral%2Fmalika-cekat.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/general/malika-tools.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgeneral%2Fmalika-tools.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/misc/investigating-errors.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmisc%2Finvestigating-errors.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/tools/custom-function.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftools%2Fcustom-function.mdx&astroContentModuleFlag=true")],
