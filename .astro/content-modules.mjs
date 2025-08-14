05721e673ec5681ceab4b528bab7ea896c8399ea
saddansyah <saddanakbar@gmail.com>
2025-08-14 10:01:48 +0700

    add: content

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,11 +1,11 @@
 
 export default new Map([
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/tools/custom-function.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftools%2Fcustom-function.mdx&astroContentModuleFlag=true")],
-["src/content/docs/general/malika-tools.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgeneral%2Fmalika-tools.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/general/malika-cekat.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgeneral%2Fmalika-cekat.mdx&astroContentModuleFlag=true")],
+["src/content/docs/general/malika-tools.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgeneral%2Fmalika-tools.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/misc/investigating-errors.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmisc%2Finvestigating-errors.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/misc/server-observability.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmisc%2Fserver-observability.mdx&astroContentModuleFlag=true")],
-["src/content/docs/tools/search.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftools%2Fsearch.mdx&astroContentModuleFlag=true")],
-["src/content/docs/tools/ongkir.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftools%2Fongkir.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/tools/custom-function.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftools%2Fcustom-function.mdx&astroContentModuleFlag=true")],
+["src/content/docs/tools/ongkir.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftools%2Fongkir.mdx&astroContentModuleFlag=true")],
+["src/content/docs/tools/search.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftools%2Fsearch.mdx&astroContentModuleFlag=true")]]);
 		
